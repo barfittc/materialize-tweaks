14bbcc24a07ab1fab80a8be0994d2a42250df3a3
CHRIS-PC\Chris <barfittc@gmail.com>
2018-03-13 16:36:17 -0400

    Don't need to check All on html creation. JS will now turn it on if all bellow it are on

--- materialize-check-all/materialize-switch-all.js
+++ materialize-check-all/materialize-switch-all.js
@@ -58,6 +58,8 @@
             }
         }
 
+
+        //console.log (!checkbox.is(':checked'), event.data, checkbox, checkbox.data("is-all"));
         event.preventDefault();
         event.stopPropagation();
     }
@@ -66,11 +68,27 @@
         var allSwitch = $(this);
         var switchName = allSwitch.attr("data-check-all");
         var switches = $(`#${switchName}`).find("[type='checkbox']");
+        var allchecked = true;
 
         switches.each(function(index, element) {
             var that = $(element);
             that.data("is-all", $(element).attr("data-check-all") === switchName);
+
+            if (!that.data("is-all") && !that.is(':checked'))
+            {
+                allchecked = false;
+            }
         });
+
+        if (allchecked)
+        {
+            switches.each(function(index, element) {
+                var that = $(element);
+                if (that.data("is-all")) {
+                    that.attr('checked', true);
+                }
+            });
+        }
 
         switches.siblings("span").on("click", switches, onCheckChanged);
     });
